26b6377c2d8d8e139424f2e7f4d56a2bf6a66c1e
jsp <jsp@humax-networks.com>
2024-02-06 19:29:20 +0900

    update run script

--- docker/chip-run-arm64/run.sh
+++ docker/chip-run-arm64/run.sh
@@ -64,8 +64,9 @@ done
 
 RUN_DIR_HOST="$here/../../"
 RUN_DIR_DOCKER="/connectedhomeip/"
-docker run --platform linux/arm64 -it "${runargs[@]}" --rm \
-    --sysctl "net.ipv6.conf.all.disable_ipv6=0 net.ipv6.conf.all.accept_ra=2" --privileged \
+docker run --platform linux/arm64 -it --rm \
+    --sysctl "net.ipv6.conf.all.disable_ipv6=0 net.ipv6.conf.all.accept_ra=1" \
+    "${runargs[@]}" --privileged \
     --mount "source=/var/run/docker.sock,target=/var/run/docker.sock,type=bind" -w "$RUN_DIR_DOCKER" -v "$RUN_DIR_HOST:$RUN_DIR_DOCKER" "$IMAGE" "$@"
 # need to add default route within container
 # ip -6 route add default via fd11:1111:1122:2222:42:acff:fe11:2 dev eth0
