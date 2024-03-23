54636feb3a8b3bd6ad3e6b6073b34608005008c0
jsp <jsp@humax-networks.com>
2024-03-23 18:46:33 +0900

    add amd64 in chip-run

---
+++ docker/chip-run/build-amd64.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+me=$(basename "$0")
+cd "$(dirname "$0")"
+
+IMAGE=chip-run-amd64
+
+[[ ${*/--help//} != "${*}" ]] && {
+    set +x
+    echo "Usage: $me <OPTIONS>
+
+  Build and (optionally tag as latest, push) a docker image from Dockerfile in CWD
+
+  Options:
+   --no-cache   passed as a docker build argument
+   --help       get this message
+"
+    exit 0
+}
+
+set -ex
+
+BUILD_ARGS=()
+if [[ ${*/--no-cache//} != "${*}" ]]; then
+    BUILD_ARGS+=(--no-cache)
+fi
+
+[[ ${*/--skip-build//} != "${*}" ]] || {
+    docker buildx build --platform linux/amd64 "${BUILD_ARGS[@]}" -t "$IMAGE" .
+    docker image prune --force
+}
+
+exit 0
