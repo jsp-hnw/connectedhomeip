7240f1e60cb5b622fed0f6d3cfca22033bfad17e
jsp <jsp@humax-networks.com>
2024-07-15 09:08:27 +0900

    update chip-run

--- docker/chip-run/entrypoint/entrypoint.sh
+++ docker/chip-run/entrypoint/entrypoint.sh
@@ -1,9 +1,34 @@
 #!/usr/bin/env bash
+
+# redirect log
+logfile=/tmp/entrypoint__.log
+exec >$logfile 2>&1
+
 mkdir -p /run/dbus
-dbus-daemon --system --fork
-/usr/lib/bluetooth/bluetoothd &
-sleep 5
+rm -f /run/dbus/pid
+
+pid=$(dbus-daemon --system --fork --print-pid)
+# wait for dbus-daemon PID
+while true; do
+    if [ -f /run/dbus/pid ]; then
+        break
+    fi
+    sleep 1
+    echo "wait for D-Bus daemon to be ready"
+done
+echo "D-Bus daemon PID: $pid"
+
+# Start bluetoothd in the background and redirect stderr to a log file
+/usr/lib/bluetooth/bluetoothd -n 2>/tmp/btlog.txt &
+# Wait for "Bluetooth management interface 1.14 initialized" in the log file
+while ! grep -q "Bluetooth management interface .* initialized" /tmp/btlog.txt; do
+    echo "wait for bluetoothd to be ready.."
+    sleep 1
+done
+echo "bluetoothd is ready"
+
 hciattach /dev/ttyS0 any 115200 noflow
 hciconfig hci0 up
+hciconfig
 
 tail -f /dev/null
